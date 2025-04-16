f69a5934ba323a032bf4c6dc1f0f960bd5f974ee
Joao Marcos Maia <joaomarcosaraujomaia14@gmail.com>
2025-04-16 13:53:28 -0300

    painel completo com CRUD de projetos integrado ao Supabase
    - Criação de novos projetos com formulário estilizado
    - Edição com dados pré-preenchidos via GET
    - Exclusão de projetos com confirmação
    - Toasts visuais com feedback automático (Bootstrap)
    - Proteção com login obrigatório usando @login_required
    - Coreção de tradução

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Home page
     path('curriculo/', views.sobre_mim, name='sobre_mim'),  # Página de currículo
-    
     path("projetos/", views.projetos, name="projetos"), ## Página de projetos
+    path('admin-projetos/', views.admin_projetos, name='admin_projetos'),
+    path('admin-projetos/criar/', views.criar_projeto, name='criar_projeto'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('admin-projetos/excluir/<int:id>/', views.excluir_projeto, name='excluir_projeto'),
+    path('admin-projetos/editar/<int:id>/', views.editar_projeto, name='editar_projeto'),
 
 ]
